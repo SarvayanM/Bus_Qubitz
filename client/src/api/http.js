c139cf4c19625bfe371e76c4d60a464e20435697
SarvayanM <sarvayan@qubitz.solutions>
2025-11-14 16:09:53 +0530

    Update http.js

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "https://bookmybus-backend2.onrender.com", // backend origin
+  baseURL: "https://bus-qubitz.onrender.com", // backend origin
   withCredentials: true,
 });
