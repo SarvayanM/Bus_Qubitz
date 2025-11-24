3a0601d1c0f7b74d705f69661264a356a988752a
SarvayanM <sarvayan@qubitz.solutions>
2025-11-24 09:44:17 +0530

    Update http.js with web service url

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: "http://localhost:4000", // backend origin
+  baseURL: "https://bus-qubitz.onrender.com", // backend origin
   withCredentials: true,
 });
